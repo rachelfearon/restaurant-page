6f7c450c5a1f4a8e6e41af1fcbf024b660e5316f
Rachel Fearon <rachel.fearon@gmail.com>
2021-09-26 13:27:35 -0400

    Setup menuItem class, display

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -4,13 +4,56 @@ export function buildMenuContent() {
     content.appendChild(menuContent);
     displayMenuItems();
 }
-
+let menuItems = [];
+//copy displayLibrary
 function displayMenuItems() {
     //display list of menu items
     const menuContainer = document.createElement('div');
     menuContainer.id = 'menuContainer';
-    const p = document.createElement('p');
-    p.textContent = 'Menu Item 1';
+
+    menuItems.forEach(item => {
+        let itemCard = item.createMenuCard(item);
+        menuContainer.append(itemCard);
+    })
+
     content.appendChild(menuContainer);
-    content.appendChild(p);
+
 }
+
+
+
+class menuItem {
+
+    constructor(name, price, description, imgUrl) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
+
+    createMenuCard() {
+        let card = document.createElement('div');
+        card.setAttribute('class', 'menuCard');
+        // card.setAttribute('style', 'white-space: pre;');
+        let title = document.createElement('h2');
+        title.textContent = `${this.name}`;
+        let image = document.createElement('img');
+        image.src = `${this.imgUrl}`;
+        let price = document.createElement('p');
+        price.textContent = `${this.price}`;
+        let description = document.createElement('p');
+        description.textContent = `${this.description}`;
+        
+        card.appendChild(title);
+        card.appendChild(price);
+        return card;
+    }
+
+}
+
+function addItemToMenu(name, price, description, imgUrl) {
+    const newItem = new menuItem(name, price, description, imgUrl);
+    menuItems.push(newItem);
+}
+
+addItemToMenu('Donut', '$9.99', 'A delicious donut', './assets/pumpkin.png');
